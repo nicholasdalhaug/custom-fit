528d412472992a9cdf7e8ae9c6b132993d4655c8
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2020-04-04 18:06:59 +0200

    Create structure and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Overview from './pages/Overview';
+import NavBar from './components/Navbar';
+import { ThemeProvider } from '@material-ui/core';
+
+import {theme} from './utils/theme'
 
 function App() {
-  return (
-    <div>
-      Main app
-    </div>
-  );
+  return <>
+    <ThemeProvider theme={theme}>
+      <NavBar title="Overview"/>
+      <Overview />
+    </ThemeProvider>
+  </>;
 }
 
 export default App;
---
+++ src/utils/theme.tsx
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#2196f3', 
+            dark: '#1565c0'
+        }
+    }
+});
