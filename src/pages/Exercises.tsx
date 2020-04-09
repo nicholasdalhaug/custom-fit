a51fa253d2d84eb35b9cb358da5761706115118d
Casper Dalhaug <casperdalhaug@gmail.com>
2020-04-09 16:04:42 +0200

    Added cards to exercises

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -1,8 +1,75 @@
 import React from 'react';
+import { Card, CardContent, Typography, CardActions, Button, makeStyles, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+const ExercisesCard = () => {
+    const classes = useStyles();
+
+    return <>
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    be*nev*o*lent
+                </Typography>
+                <Typography color="textSecondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    </>
+}
 
 const Exercises = () => {
     return <>
-        Hello world
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <ExercisesCard />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <ExercisesCard />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <ExercisesCard />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <ExercisesCard />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <ExercisesCard />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <ExercisesCard />
+            </Grid>
+            <Grid item xs={6} sm={3}>
+                <ExercisesCard />
+            </Grid>
+        </Grid>
     </>
 }
 
